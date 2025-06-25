49be65f034a3149edc922cd2c63f043b9ac3d366
XaveriusYehuda <xyehuda3@gmail.com>
2025-06-25 11:35:52 +0700

    perbaikan pwa manifest

---
+++ docs/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://xaveriusyehuda.github.io/story-app-pwa/sw.js', { scope: 'https://xaveriusyehuda.github.io/story-app-pwa/' })})}
